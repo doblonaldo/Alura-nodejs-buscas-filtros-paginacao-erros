5036dabc01de5c9d5406ba2553d6a0366085230e
doblonaldo <douglasbacosta@outlook.com>
2023-11-02 04:27:19 -0300

    Aula-05 (config Eslint)

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 mongoose.connect(process.env.DB_CONNECTION_STRING);
 
 let db = mongoose.connection;
